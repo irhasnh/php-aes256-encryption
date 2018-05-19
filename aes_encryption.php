4cf2d8c35e8e51ae4d47f90ba299ee56875800ac
Trisna Novi Ashari <trisna.x2@gmail.com>
2018-05-19 15:45:29 +0700

    Update encryption function

--- aes_encryption.php
+++ aes_encryption.php
@@ -3,22 +3,30 @@
 function _encrypt($message, $hmac = FALSE)
 {
 	date_default_timezone_set('UTC');
-	$secret 									= 'T6h474MagTHgXCKBtASxDqmnB==yU067';
+	$secret 									= '474MaXCKBtASxDT6hgyU067THgqmnB==';
 	$encryptionMethod 							= 'AES-256-CBC';
 	$iv 										= substr(bin2hex(openssl_random_pseudo_bytes(16)),0,16);
 	$encrypted 									= base64_encode($iv) . openssl_encrypt($message, $encryptionMethod, $secret, 0, $iv);
 
 	return $encrypted;
-	
+
 }
 
 function _decrypt($message, $hmac = FALSE)
 {
 	date_default_timezone_set('UTC');
-	$secret 									= 'T6h474MagTHgXCKBtASxDqmnB==yU067';
+	$secret 									= '474MaXCKBtASxDT6hgyU067THgqmnB==';
 	$encryptionMethod 							= 'AES-256-CBC';
 	$iv 										= base64_decode(substr($message, 0, 24));
 
 	return openssl_decrypt(substr($message, 24), $encryptionMethod, $secret, 0, $iv);
 
 }
+
+$message			= 'This is a test message.';
+$encrypted_message	= _encrypt($message);
+$decrypted_message	= _decrypt($encrypted_message);
+
+echo 'Message: ' . $message . "\n";
+echo 'Encrypted message: ' . $encrypted_message . "\n";
+echo 'Derypted message: ' . $decrypted_message . "\n";
